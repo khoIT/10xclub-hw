065781afbc972b4a4131786ed01051900531a70e
Khoi Tran <ktran@euclidanalytics.com>
2018-02-15 12:52:32 -0800

    add all files

---
+++ stock_price_maximize.py
@@ -0,0 +1,22 @@
+'''Stock Buy Sell to Maximize Profit. Difficulty: 3.1
+The cost of a stock on each day is given in an array, find the max profit that you can make by buying and selling in those days.
+For example, if the given array is
+[100, 180, 260, 310, 40, 535, 695],
+the maximum profit can earned by buying on day 0, selling on day 3.
+Again buy on day 4 and sell on day 6.
+
+If the given array of prices is sorted in decreasing order, then profit cannot be earned at all.
+'''
+def max_stock_price(arr):
+    high = arr[0]
+    low = arr[0]
+    max_distance = high - low
+    for price in arr:
+        if price < low:
+            low = price
+            high = price
+        if price > high:
+            high = price
+            if high - low > max_distance:
+                max_distance = high-low
+    return max_distance

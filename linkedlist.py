04bca85f2adba2a4bccfecd0cbae91382e0f9eb6
Khoi Tran <ktran@euclidanalytics.com>
2018-02-21 13:59:43 -0800

    implement linkedlist database

---
+++ linkedlist.py
@@ -0,0 +1,14 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_start(self, value):
+    def add_end(self, value):
+    def add_at

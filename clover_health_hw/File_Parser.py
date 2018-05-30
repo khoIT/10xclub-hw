294e3c5ed72b837b7395732ca28ff419cd169f3a
Khoi T <emails.khoi@gmail.com>
2018-05-29 21:09:52 -0700

    more tests cases for data invalid

--- clover_health_hw/File_Parser.py
+++ clover_health_hw/File_Parser.py
@@ -1,54 +1,84 @@
-
 class TableSpec(object):
-    def __init__(self, file_name, specs):
+    def __init__(self, file_name, column_specs):
         # represent a table in db, contains list of ColumnSpec
         self.name = file_name
-        self.specs = specs
+        self.specs = column_specs
 
 class ColumnSpec(object):
     def __init__(self, column_name, width, datatype):
         # contain information for one column in table
         self.column_name = column_name
-        self.width = width
+        self.width = int(width)
         self.datatype = datatype.title()
 
 class DataRow(object):
-    def __init__(self, row, table_spec):
-        self.row = []
+    def __init__(self, data_row, table_spec):
+        # dynamically parse row data based on column_specs in table_spec
+        row = self.__parse_row(data_row, table_spec)
+        if not row:
+            raise ValueError('Could not parse data_row')
+        self.row = row
+
+    def __parse_row(self, data_row, table_spec):
         idx = 0
-        while idx < len(table_spec.specs) and idx < len(row):
+        row = []
+        if len(data_row) != len(table_spec.specs):
+            print("Length of row and numbers of column don't match for table {} row {}".format(table_spec.name, data_row))
+            return None
+
+        while idx < len(table_spec.specs) and idx < len(data_row):
             spec = table_spec.specs[idx]
+            data = data_row[idx]
             try:
                 if spec.datatype == 'Boolean':
-                    self.row.append(int(row[idx]))
+                    row.append(int(data))
+                elif spec.datatype == 'Integer':
+                    if len(data) > spec.width:
+                        print("Length of data exceed column's width")
+                        return None
+                    row.append(int(data))
+                elif spec.datatype == 'Text':
+                    row.append(data)
                 else:
-                    self.row.append(row[idx])
+                    print("Unrecognized data type: {}".format(spec.datatype))
+                    return None
             except ValueError as err:
                 print(err)
-
             idx += 1
+        return row
 
 class DataFile(object):
-    def __init__(self, file_path, table_class, table_spec, rows):
+    def __init__(self, file_path, table_class, rows):
         self.table_class = table_class
-        self.rows = []
-        for row in rows:
-            self.rows.append(DataRow(row, table_spec))
+        self.rows = rows
+
+    def add_row(self, data_row):
+        if type(data_row) == DataRow:
+            self.rows.append(data_row)
+        else:
+            print("Can only work with DataRow type")
+
+import csv
 
 def parseDataFile(file_path, table_class, table_spec):
-    columns = []
+    rows = []
     with open(file_path,'r') as df:
-        for line in df:
-            columns.append(line.strip().split())
+        for line in csv.reader(df):
+            try:
+                data_row = DataRow(line[0].split(), table_spec)
+            except ValueError as err:
+                continue
+            rows.append(data_row)
     df.close()
-    return DataFile(file_path, table_class, table_spec, columns)
+    return DataFile(file_path, table_class, rows)
 
 def parseSpecsFile(file_path):
     columns = []
     with open(file_path,'r') as sf:
-        headers = sf.readline().strip().split(',')
-        for line in sf:
-            columns.append(line.strip().split(','))
+        reader = csv.reader(sf)
+        headers = next(reader)
+        for line in reader:
+            columns.append(line)
     sf.close()
     return columns
 
